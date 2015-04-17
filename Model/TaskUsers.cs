a395ae6ae309e73d0d32b130f380bd8df368b1dc
fatagun <fatagun@gmail.com>
2015-04-17 14:17:44 +0300

    taksusers

--- Model/TaskUsers.cs
+++ Model/TaskUsers.cs
@@ -1,7 +1,14 @@
-﻿namespace Model
+﻿using System;
+
+namespace Model
 {
     public class TaskUser : BaseEntity
     {
+        public TaskUser()
+        {
+            DateCreated = DateTime.Now;
+        }
+
         public string UserId { get; set; }
     }
 }
